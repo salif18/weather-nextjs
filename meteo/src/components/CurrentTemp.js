b60024c5e6499e803cdc8640e14c79fe7ceece04
Your Name <you@example.com>
2023-10-15 14:34:20 +0000

    ferst

--- meteo/src/components/CurrentTemp.js
+++ meteo/src/components/CurrentTemp.js
@@ -2,14 +2,14 @@ import React from 'react';
 import moment from 'moment';
 const CurrentTemp = ({day}) => {
     return (
-        <article className='current-temp'>
+        <article className='today'>
             <figure>
             <img src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`} className='w-icon' alt='weather icon' />
             </figure>
             <section className='other'>
-              <section className='day'>{moment(day.dt * 1000).format('ddd')}</section>
-              <section className='temp'>{(day.temp.night/10).toFixed(2)}&#176; C</section>
-              <section className='temp'>{(day.temp.day/10).toFixed(2)}&#176; C</section>
+            <section className='day'>{moment(day.dt * 1000).format('ddd')}</section>
+              <section className='temp'>Nuit {(day.temp.night/10).toFixed(2)}&#176;C</section>
+              <section className='temp'>Jour {(day.temp.day/10).toFixed(2)}&#176;C</section>
             </section>
         </article>
     );
--- meteo/src/components/Weather.js
+++ meteo/src/components/Weather.js
@@ -9,9 +9,8 @@ const Weather = ({day}) => {
          <figure>
            <img className="w-icon" src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`} alt="weather icon" />
         </figure>
-        
-         <section className='temp'>{(day.temp.night/10).toFixed(2)}&#176; C</section>
-         <section className='temp'>{(day.temp.day/10).toFixed(2)}&#176; C</section>
+         <section className='temp'>Nuit {(day.temp.night/10).toFixed(2)}&#176;C</section>
+         <section className='temp'>Jour {(day.temp.day/10).toFixed(2)}&#176;C</section>
         </article>
     );
 }
